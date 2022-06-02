b93b7496a2f4480127ae4cbdc8329139c6e8b8ad
Erick Mayorga <erick.mayorga@epn.edu.ec>
2022-06-01 22:55:36 -0500

    scene1 update3

--- Assets/Scripts/ChangeScore.cs
+++ Assets/Scripts/ChangeScore.cs
@@ -6,20 +6,17 @@ using UnityEngine.UI;
 public class ChangeScore : MonoBehaviour
 {
 
-    public Text textScore;
+    //public Text textScore;
     // Start is called before the first frame update
     
     // activamos el trigger de la caja 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // si la colisión es  con el jugador
+        //Debug.Log(collision.tag);
         if (collision.tag == "Player") {
-            Score.points = Score.points + 1;
-            textScore.text = Score.points.ToString(); // obtenga el puntaje
             GetComponent <SpriteRenderer>().enabled = false;
             gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
             Destroy(gameObject, 0.2f);
-
         }
     }
 }
